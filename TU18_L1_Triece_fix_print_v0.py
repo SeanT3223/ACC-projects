1bd9e48ad14ed340098633a63f630fbf3cb0e9fb
SeanT3223 <160192304+SeanT3223@users.noreply.github.com>
2024-02-23 15:40:19 -0600

    Add files via upload

---
+++ TU18_L1_Triece_fix_print_v0.py
@@ -0,0 +1,122 @@
+# Name: Sean Triece
+# TU18,COSC1336, Lab 1, Fix print syntax errors
+#
+# A first program usually prints out a few words, like: Hello, world!
+# It should be easy. But when learning a brand new language, even
+# something "easy" can be difficult. Find and fix any errors below.
+#
+# This program contains many errors. Most are: SyntaxError
+# Repair each output statement below so it outputs correctly.
+# There should be a single space between words in your output.
+# Each statement below, when repaired, displays some text.
+# If the statement is written in non-Python, convert it into Python, using print()
+#
+# Statements can be repaired with the proper use of the print function
+# and the proper use of string deliminators. Delimiters such as: ' or "
+# A delimiter limits something - it/they indicate where something starts and stops.
+# Delimiters often come in pairs, like: 'x' "x" (x) <x> {x} [x] \x\  /x/ .x. ,x, #x# |x|
+#
+# IF IT IS NOT BROKEN, DON'T FIX IT.
+# Make fixes only if needed with the least amount of editing possible.
+# When in doubt, look at what the string says. If it says:
+# "Here are four double-quote marks:", then, after the text, it should display: """"
+# Note the difference between double-quote " and single-quote '
+# A correct solution will show multiple lines; Each new line starts with a number, in order 1..47
+
+print ("1. Start of Lab 1 to fix print statements...")
+print ("2. let's get started")
+print ("3. does this work?")
+print ("4. working or not")
+print ("5. how about this?")
+print ("6. is this right?")
+print ("7. Can you see me now?")
+print ("8. Any problem leaving this indented to the right? Move to fix if needed.")
+print ("9. how many errors can you find?")
+print ('10. success, I think')
+print ("11. What if I put a period at the end?")
+print ("12. What if I put a semicolon at the end?")
+print ("13. What could possibly be wrong?")
+print ("14. this","that", "and", "the", "other")
+print ("15. one", "two", "three")  # keep these 3 words on one line of output
+print ("16. tic", "\n17. tak", "\n18. toe") # keep all 3 statements on 1 line of code, but print 3 separate lines of output
+print ("19. one, two, buckle, my, shoe") #keep the commas, keep as 1 print statement on 1 line of code, print on one line of output
+print ("20. anybody knows that prnt means print")
+print('21. I cannot wait any longer')
+print ("22. good grief! This is messed up, too!")
+print("23. try it this way!!")
+print("24. What about this method?")
+print("25. Does it matter where the verb goes?")
+print("26. Start","End")
+print("27. give me a shout when it works")
+print ("28. don't mess up on me now!")
+print("29. Please just do what I say!")
+print("30. The best is yet to come.")
+print("31. This works in Java - how about Python?")
+print(f"32. This works in C, what about Python?")
+print("33. In C++, this works. What about in Python?")
+print            ("34. Can I do this??")
+print   ("35. If this were easy, anyone could do it!")
+print("36. Quote the raven: \"Nevermore\" ")  # put the "Nevermore" in quotes
+print('37. She said: \"Where\'s my cell phone? I\'ve got to find it!\"')
+print("38. Elvis sang, \"Don't step on my blue suede shoes.\", in his younger days.")
+print("39. Here are four double-quote marks: \"\"\"\"")
+print("40. Here are four single-quote marks: ''''")
+print("41. How could something so easy be so difficult?")
+print("42. does it matter uppercase or lowercase?")
+print('43. this ought to work')
+print ("44. One letter at a time.")
+out = "45. That is about enough of this."# set out to a string variable, then print the variable: out
+print(out) 
+print("46. Good-bye, and good luck.")
+print ("47. ... end of Lab 1 to fix print statements...")
+
+# Paste your output below:
+##1. Start of Lab 1 to fix print statements...
+##2. let's get started
+##3. does this work?
+##4. working or not
+##5. how about this?
+##6. is this right?
+##7. Can you see me now?
+##8. Any problem leaving this indented to the right? Move to fix if needed.
+##9. how many errors can you find?
+##10. success, I think
+##11. What if I put a period at the end?
+##12. What if I put a semicolon at the end?
+##13. What could possibly be wrong?
+##14. this that and the other
+##15. one two three
+##16. tic 
+##17. tak 
+##18. toe
+##19. one, two, buckle, my, shoe
+##20. anybody knows that prnt means print
+##21. I cannot wait any longer
+##22. good grief! This is messed up, too!
+##23. try it this way!!
+##24. What about this method?
+##25. Does it matter where the verb goes?
+##26. Start End
+##27. give me a shout when it works
+##28. don't mess up on me now!
+##29. Please just do what I say!
+##30. The best is yet to come.
+##31. This works in Java - how about Python?
+##32. This works in C, what about Python?
+##33. In C++, this works. What about in Python?
+##34. Can I do this??
+##35. If this were easy, anyone could do it!
+##36. Quote the raven: "Nevermore" 
+##37. She said: "Where's my cell phone? I've got to find it!"
+##38. Elvis sang, "Don't step on my blue suede shoes.", in his younger days.
+##39. Here are four double-quote marks: """"
+##40. Here are four single-quote marks: ''''
+##41. How could something so easy be so difficult?
+##42. does it matter uppercase or lowercase?
+##43. this ought to work
+##44. One letter at a time.
+##45. That is about enough of this.
+##46. Good-bye, and good luck.
+##47. ... end of Lab 1 to fix print statements...
+# Hint: You can "comment out" your output using:
+# Format -> Comment Out Region
